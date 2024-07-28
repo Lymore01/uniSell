b466e28be07d35ba2cb05aa671c1fda0e63a7891
Kelly Limo <130097627+Lymore01@users.noreply.github.com>
2024-07-28 19:45:28 +0300

    added more features

--- src/components/shared/TopNav.tsx
+++ src/components/shared/TopNav.tsx
@@ -3,6 +3,7 @@ import { MdDarkMode } from "react-icons/md";
 import { useContext } from "react";
 import { MdLightMode } from "react-icons/md";
 import { ThemeContext } from "../../contexts/ThemeProvider";
+import moment from "moment";
 
 const TopNav = ({ title }: { title: string }) => {
   const [theme, setTheme] = useContext(ThemeContext);
@@ -10,7 +11,7 @@ const TopNav = ({ title }: { title: string }) => {
   return (
     <section className="w-full flex items-center justify-between flex-row p-2 rounded-lg text-secondary">
       <div className="flex flex-col">
-        <p className="text-xs font-mono">12:45 pm,</p>
+        <p className="text-xs font-mono">{moment().format('LT')},</p>
         <h1 className="font-semibold text-xl">{title} 🎉</h1>
       </div>
       <div className="flex space-x-6 items-end justify-end">
